c24eee1653c31d97e3b6811fd53e49c1033a419e
neo-askin <issayamilrt@gmail.com>
2023-05-13 13:08:16 +0200

    fix: scale image for export

--- src/components/TemplateManager.svelte
+++ src/components/TemplateManager.svelte
@@ -3,6 +3,7 @@
 	import { jsPDF } from "jspdf";
 	import domtoimage from 'dom-to-image';
 
+
 	let pdf;
 
 
@@ -29,7 +30,7 @@
 
 
 	async function pdfExport() {
-	//front side
+  //front side
   const content = document.getElementById('front');
   const imgData = await domtoimage.toPng(content);
 
@@ -38,21 +39,36 @@
   const pdfWidth = pdf.internal.pageSize.getWidth()/2;
   const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
 
-	//add the first image to the pdf file
+  //add the first image to the pdf file
   pdf.addImage(imgData, 'PNG', 20, 20, pdfWidth, pdfHeight);
 
   //back side
-	const content2 = document.getElementById('back');
+  const content2 = document.getElementById('back');
   const imgData2 = await domtoimage.toPng(content2);
+
   const imgProps2 = pdf.getImageProperties(imgData2);
   const pdfWidth2 = pdf.internal.pageSize.getWidth()/2;
   const pdfHeight2 = (imgProps2.height * pdfWidth2) / imgProps2.width;
 
-	//add the second image to the pdf file
-  pdf.addImage(imgData2, 'PNG', 20, pdf.internal.pageSize.getHeight() - pdfHeight2 - 20, pdfWidth2, pdfHeight2);
+  //add the second image to the pdf file
+  const canvas = document.createElement('canvas');
+  canvas.width = imgProps2.width;
+  canvas.height = imgProps2.height;
+  const ctx = canvas.getContext('2d');
+  const img = new Image();
+  img.onload = function() {
+    ctx.save();
+    ctx.translate(canvas.width, 0);
+    ctx.scale(-1, 1); // flip horizontally
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    ctx.restore();
 
-	//download the pdf file
-  pdf.save('card.pdf');
+    pdf.addImage(canvas.toDataURL(), 'PNG', 20, pdf.internal.pageSize.getHeight() - pdfHeight2 - 20, pdfWidth2, pdfHeight2);
+
+    //download the pdf file
+    pdf.save('card.pdf');
+  };
+  img.src = imgData2;
 }
 
 
